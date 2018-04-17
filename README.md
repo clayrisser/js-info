4916a1defa2d6b3aa6d5b36f4dc756bed9cba2a2
Jam Risser <jam@jamrizzi.com>
2018-04-17 04:05:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,77 @@
+# cross-environment
+
+[![GitHub stars](https://img.shields.io/github/stars/codejamninja/cross-environment.svg?style=social&label=Stars)](https://github.com/codejamninja/cross-environment)
+
+> Comprehensive cross platform way to get environment details
+
+Please ★ this repo if you found it useful ★ ★ ★
+
+
+## Features
+
+* Completely cross platform
+* Get runtime (node, browser)
+* Determine if mobile device
+* Get os (darwin, win32, linux, freebsd, openbsd, kindle)
+* Get JavaScript engine (gecko, webkit, v8, jsc)
+* Get environment (development, testing, staging, production)
+* Get browser (chrome, firefox, opera, safari, edge, ie)
+* Built in memoization
+
+
+## Installation
+
+```sh
+npm install --save cross-environment
+```
+
+
+## Dependencies
+
+* [NodeJS](https://nodejs.org)
+
+
+## Usage
+
+```sh
+import crossEnvironment from 'cross-environment';
+
+console.log(crossEnvironment());
+```
+
+
+## Support
+
+Submit an [issue](https://github.com/codejamninja/cross-environment/issues/new)
+
+
+## Contributing
+
+Review the [guidelines for contributing](https://github.com/codejamninja/cross-environment/blob/master/CONTRIBUTING.md)
+
+
+## License
+
+[MIT License](https://github.com/codejamninja/cross-environment/blob/master/LICENSE)
+
+[Jam Risser](https://codejam.ninja) © 2018
+
+
+## Changelog
+
+Review the [changelog](https://github.com/codejamninja/cross-environment/blob/master/CHANGELOG.md)
+
+
+## Credits
+
+* [Jam Risser](https://codejam.ninja) - Author
+
+
+## Support on Liberapay
+
+A ridiculous amount of coffee ☕ ☕ ☕ was consumed in the process of building this project.
+
+[Add some fuel](https://liberapay.com/codejamninja/donate) if you'd like to keep me going!
+
+[![Liberapay receiving](https://img.shields.io/liberapay/receives/codejamninja.svg?style=flat-square)](https://liberapay.com/codejamninja/donate)
+[![Liberapay patrons](https://img.shields.io/liberapay/patrons/codejamninja.svg?style=flat-square)](https://liberapay.com/codejamninja/donate)
---
+++ src/index.js
@@ -0,0 +1,162 @@
+import _ from 'lodash';
+
+const { env, argv } = process;
+const customEnvironment = {};
+// eslint-disable-next-line no-undef
+const browserWindow = typeof window === 'undefined' ? null : window;
+
+export default function nodeEnvironment() {
+  return {
+    runtime: getRuntime(),
+    mobile: isMobile(),
+    os: getOS(),
+    engine: getEngine(),
+    env: getEnv(),
+    browser: getBrowser()
+  };
+}
+
+export function setEnvironment(environment) {
+  _.memoize.Cache = WeakMap;
+  _.assign(customEnvironment, environment);
+}
+
+function _getRuntime() {
+  if (customEnvironment.runtime) return customEnvironment.runtime;
+  if (isBrowser()) return 'browser';
+  if (isNode()) return 'node';
+  return 'unknown';
+}
+export const getRuntime = _.memoize(_getRuntime);
+
+function _isMobile() {
+  if (customEnvironment.mobile) return customEnvironment.mobile;
+  if (getRuntime() === 'node') return false;
+  if (!browserWindow) return false;
+  return /mobile/i.test(_.get(browserWindow, 'navigator.userAgent', ''));
+}
+export const isMobile = _.memoize(_isMobile);
+
+function _getOS() {
+  if (customEnvironment.os) return customEnvironment.os;
+  if (getRuntime() === 'node') {
+    return process.platform;
+  }
+  if (!browserWindow) return 'unknown';
+  const userAgent = _.get(browserWindow, 'navigator.userAgent', '');
+  if (/mac\sos\sx/i.test(userAgent)) {
+    return 'darwin';
+  } else if (/freebsd/i.test(userAgent)) {
+    return 'freebsd';
+  } else if (/openbsd/i.test(userAgent)) {
+    return 'openbsd';
+  } else if (/linux/i.test(userAgent)) {
+    return 'linux';
+  } else if (/windows/i.test(userAgent)) {
+    return 'win32';
+  } else if (/kindle/i.test(userAgent)) {
+    return 'kindle';
+  }
+  return 'unknown';
+}
+export const getOS = _.memoize(_getOS);
+
+function _getEngine() {
+  if (customEnvironment.engine) return customEnvironment.engine;
+  if (getRuntime() === 'node') return 'v8';
+  if (!browserWindow) return 'unknown';
+  let engine = 'unknown';
+  const userAgent = _.get(browserWindow, 'navigator.userAgent', '');
+  if (/gecko/i.test(userAgent)) {
+    engine = 'gecko';
+  } else if (/webkit/i.test(userAgent)) {
+    engine = 'webkit';
+  }
+  try {
+    throw new Error();
+  } catch (exc) {
+    if (_.isNumber(exc.sourceId)) {
+      engine = 'jsc';
+    }
+  }
+  const v8string =
+    'function%20javaEnabled%28%29%20%7B%20%5Bnative%20code%5D%20%7D';
+  if (browserWindow.devicePixelRatio) {
+    // eslint-disable-next-line no-undef
+    if (
+      engine === 'webkit' &&
+      escape(browserWindow.navigator.javaEnabled.toString()) === v8string
+    ) {
+      return 'v8';
+    } else if (engine === 'jsc') {
+      return 'jsc';
+    }
+  }
+  return 'unknown';
+}
+export const getEngine = _.memoize(_getEngine);
+
+function _getBrowser() {
+  if (customEnvironment.browser) return customEnvironment.browser;
+  if (getRuntime() !== 'browser') return null;
+  const userAgent = _.get(browserWindow, 'navigator.userAgent', '');
+  if (/chrome/i.test(userAgent)) {
+    return 'chrome';
+  } else if (/firefox/i.test(userAgent)) {
+    return 'firefox';
+  } else if (/safari/i.test(userAgent)) {
+    return 'safari';
+  } else if (/opera/i.test(userAgent)) {
+    return 'opera';
+  } else if (/edge/i.test(userAgent)) {
+    return 'edge';
+  }
+  let ieVersion = -1;
+  if (
+    browserWindow.navigator.appName.toLowerCase() ===
+    'microsoft internet explorer'
+  ) {
+    const ieRegex = new RegExp('MSIE ([0-9]{1,}[.0-9]{0,})');
+    if (ieRegex.exec(userAgent) !== null) ieVersion = parseFloat(RegExp.$1);
+  } else if (browserWindow.navigator.appName.toLowerCase() === 'netscape') {
+    const ieRegex = new RegExp('Trident/.*rv:([0-9]{1,}[.0-9]{0,})');
+    if (ieRegex.exec(userAgent) !== null) ieVersion = parseFloat(RegExp.$1);
+  }
+  if (ieVersion > -1) {
+    return `ie${ieVersion}`;
+  }
+  return 'unknown';
+}
+export const getBrowser = _.memoize(_getBrowser);
+
+function _getEnv() {
+  if (customEnvironment.env) return customEnvironment.env;
+  const nodeEnv = (env.NODE_ENV || '').toLowerCase();
+  let environment = 'development';
+  if (_.get(env, '__DEV__', '').toLowerCase() === 'false') {
+    environment = 'production';
+  }
+  if (_.includes(argv, '--test') || _.includes(argv, '--testing')) {
+    return 'testing';
+  } else if (_.includes(argv, '--stage') || _.includes(argv, '--staging')) {
+    return 'staging';
+  } else if (_.includes(argv, '--prod') || _.includes(argv, '--production')) {
+    return 'production';
+  } else if (nodeEnv === 'test' || nodeEnv === 'testing') {
+    return 'testing';
+  } else if (nodeEnv === 'stage' || nodeEnv === 'staging') {
+    return 'staging';
+  } else if (nodeEnv === 'prod' || nodeEnv === 'production') {
+    return 'production';
+  }
+  return environment;
+}
+export const getEnv = _.memoize(_getEnv);
+
+const isBrowser = new Function(
+  'try { return this === window } catch (e) { return false }'
+);
+
+const isNode = new Function(
+  'try { return this === global } catch (e) { return false }'
+);
