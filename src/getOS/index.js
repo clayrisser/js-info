eddc3b4374174bb8bcead48f3915213f963ce08d
Jam Risser <jam@codejam.ninja>
2018-07-10 21:40:42 +0530

    Improved os detection

--- src/getOS/index.js
+++ src/getOS/index.js
@@ -1,22 +1,73 @@
 import get from 'lodash/get';
-import { runtime } from '../';
+import { runtime } from '..';
 
 export default function getOS() {
-  if (runtime.node) return process.platform;
+  if (runtime.node) {
+    const release = getRelease();
+    if (/ubuntu/i.test(release)) {
+      return 'ubuntu';
+    } else if (/debian/i.test(release)) {
+      return 'debian';
+    } else if (/centos/i.test(release)) {
+      return 'centos';
+    } else if (/fedora/i.test(release)) {
+      return 'fedora';
+    } else if (/red\shat/i.test(release)) {
+      return 'redhat';
+    }
+    if (process.platform && process.platform.length) {
+      return process.platform;
+    }
+    return 'unknown';
+  }
   if (typeof window === 'undefined') return 'unknown';
   const userAgent = get(window, 'navigator.userAgent', '');
-  if (/mac\sos\sx/i.test(userAgent)) {
+  if (/windows/i.test(userAgent)) {
+    return 'win32';
+  } else if (/ipad|iphone|ipod/i.test(userAgent)) {
+    return 'ios';
+  } else if (/mac\sos\sx/i.test(userAgent)) {
     return 'darwin';
+  } else if (/android/i.test(userAgent)) {
+    return 'android';
+  } else if (/debian/i.test(userAgent)) {
+    return 'debian';
+  } else if (/ubuntu/i.test(userAgent)) {
+    return 'ubuntu';
+  } else if (/centos/i.test(userAgent)) {
+    return 'centos';
+  } else if (/fedora/i.test(userAgent)) {
+    return 'fedora';
+  } else if (/red\shat/i.test(userAgent)) {
+    return 'redhat';
+  } else if (/slackware/i.test(userAgent)) {
+    return 'slackware';
+  } else if (/linux/i.test(userAgent)) {
+    return 'linux';
   } else if (/freebsd/i.test(userAgent)) {
     return 'freebsd';
   } else if (/openbsd/i.test(userAgent)) {
     return 'openbsd';
-  } else if (/linux/i.test(userAgent)) {
-    return 'linux';
-  } else if (/windows/i.test(userAgent)) {
-    return 'win32';
-  } else if (/kindle/i.test(userAgent)) {
-    return 'kindle';
+  } else if (/sunos/i.test(userAgent)) {
+    return 'sunos';
+  } else if (/aix/i.test(userAgent)) {
+    return 'aix';
+  } else if (/amigaos/i.test(userAgent)) {
+    return 'amigaos';
+  } else if (/beos/i.test(userAgent)) {
+    return 'beos';
+  } else if (/star-blade/i.test(userAgent)) {
+    return 'star-blade';
+  } else if (/nintendo\swii/i.test(userAgent)) {
+    return 'wii';
+  } else if (/nintendo/i.test(userAgent)) {
+    return 'nintendo';
   }
   return 'unknown';
+}
+
+function getRelease() {
+  const deasync = require('deasync');
+  const exec = deasync(require('child_process').exec);
+  return exec('cat /etc/os-release');
 }
--- src/os.js
+++ src/os.js
@@ -3,12 +3,40 @@ import memoize from 'lodash/memoize';
 let getOS = null;
 
 class OS {
+  get aix() {
+    return this.value === 'aix';
+  }
+
+  get amigaos() {
+    return this.value === 'amigaos';
+  }
+
   get android() {
     return this.value === 'android';
   }
 
+  get beos() {
+    return this.value === 'beos';
+  }
+
+  get bsd() {
+    return this.freebsd || this.openbsd;
+  }
+
+  get centos() {
+    return this.value === 'centos';
+  }
+
   get darwin() {
-    return this.value === 'darwin';
+    return this.value === 'darwin' || this.ios;
+  }
+
+  get debian() {
+    return this.ubuntu || this.value === 'debian';
+  }
+
+  get fedora() {
+    return this.value === 'fedora';
   }
 
   get freebsd() {
@@ -17,14 +45,28 @@ class OS {
 
   get info() {
     return {
+      aix: this.aix,
+      amigaos: this.amigaos,
       android: this.android,
+      beos: this.beos,
+      bsd: this.bsd,
+      centos: this.centos,
       darwin: this.darwin,
+      debian: this.debian,
+      fedora: this.fedora,
       freebsd: this.freebsd,
       ios: this.ios,
-      kindle: this.kindle,
       linux: this.linux,
       mac: this.mac,
+      nintendo: this.nintendo,
       openbsd: this.openbsd,
+      redhat: this.redhat,
+      rhel: this.rhel,
+      slackware: this.slackware,
+      starBlade: this.starBlade,
+      sunos: this.sunos,
+      ubuntu: this.ubuntu,
+      unix: this.unix,
       value: this.value,
       win: this.win,
       win32: this.win32,
@@ -36,25 +78,76 @@ class OS {
     return this.value === 'ios';
   }
 
-  get kindle() {
-    return this.value === 'kindle';
-  }
-
   get linux() {
-    return this.value === 'linux';
+    if (this.rhel) return true;
+    if (this.debian) return true;
+    switch (this.value) {
+      case 'linux':
+        return true;
+      case 'android':
+        return true;
+      case 'slackware':
+        return true;
+    }
+    return false;
   }
 
   get mac() {
     return this.darwin;
   }
 
+  get nintendo() {
+    return this.value === 'nintendo' || this.wii;
+  }
+
   get openbsd() {
     return this.value === 'openbsd';
+  }
+
+  get redhat() {
+    return this.value === 'redhat';
+  }
+
+  get rhel() {
+    return this.redhat || this.centos || this.fedora;
+  }
+
+  get slackware() {
+    return this.value === 'slackware';
+  }
+
+  get starBlade() {
+    return this.value === 'star-blade';
+  }
+
+  get sunos() {
+    return this.value === 'sunos';
+  }
+
+  get ubuntu() {
+    return this.value === 'ubuntu';
+  }
+
+  get unix() {
+    if (this.linux) return true;
+    if (this.darwin) return true;
+    if (this.bsd) return true;
+    switch (this.value) {
+      case 'ais':
+        return true;
+      case 'sunos':
+        return true;
+    }
+    return false;
   }
 
   get value() {
     if (!getOS) getOS = memoize(require('./getOS').default);
     return getOS();
+  }
+
+  get wii() {
+    return this.value === 'wii';
   }
 
   get win() {
